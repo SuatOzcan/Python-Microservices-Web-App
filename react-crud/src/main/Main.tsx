cab0ddb0f8030ee25cce7a2c2733712ed784c6e0
Suat Ozcan <41187131+SuatOzcan@users.noreply.github.com>
2021-06-26 13:03:34 +0300

    Build Main.

--- react-crud/src/main/Main.tsx
+++ react-crud/src/main/Main.tsx
@@ -1,51 +1,76 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Product } from "../Interfaces/product";
 
 const Main = () => {
+
+  const [products, setProducts] = useState([] as Product[]);
+
+  useEffect( () =>{
+    ( 
+      async () => {
+        const response = await fetch(`http://localhost:8000/api/products`);
+        const data = await response.json();
+        setProducts(data);
+      }
+    )();
+
+  },[]);
+      
+    const like = async (id:number) => {
+          await fetch(`http://localhost:8000/api/products/${id}/like`,
+                       {method:`POST`,
+                        headers:{'Content-Type':'application/json'}
+                      });
+
+                      setProducts(products.map(
+                        (p:Product) => {
+                          if(p.id === id){
+                            p.likes++;
+                          }
+                          return p;
+                        }
+                      ))
+    }    
+
     return(
-       <div>
+
 
 <main>
-
-  <section className="py-5 text-center container">
-    <div className="row py-lg-5">
-      <div className="col-lg-6 col-md-8 mx-auto">
-        <h1 className="fw-light">Album example</h1>
-        <p className="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
-        <p>
-          <a href="#" className="btn btn-primary my-2">Main call to action</a>
-          <a href="#" className="btn btn-secondary my-2">Secondary action</a>
-        </p>
-      </div>
-    </div>
-  </section>
 
   <div className="album py-5 bg-light">
     <div className="container">
 
       <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-        <div className="col">
-          <div className="card shadow-sm">
-    
-
-            <div className="card-body">
-              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <div className="d-flex justify-content-between align-items-center">
-                <div className="btn-group">
-                  <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                </div>
-                <small className="text-muted">9 mins</small>
-              </div>
-            </div>
-          </div>
-        </div>
+       {products.map(
+         (p:Product) => {
+           return(      
+           <div className="col" key = {p.id}>
+           <div className="card shadow-sm">
+             <img src = {p.image} height = "180"/>
+             <div className="card-body">
+               <p className="card-text">{p.title}</p>
+               <div className="d-flex justify-content-between align-items-center">
+                 <div className="btn-group">
+                   <button type="button" className="btn btn-sm btn-outline-secondary"
+                   onClick={() => like(p.id)}
+                   >Like</button>
+                 </div>
+                 <small className="text-muted">{p.likes} likes</small>
+               </div>
+             </div>
+           </div>
+         </div>
+         )
+         }
+       )}
       </div>
     </div>
   </div>
 
 </main>
 
-       </div>
     );
 };
 
